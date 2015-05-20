d4d5de5e58b324a8d5488bc9f49395a6d29e14fe
Michael Hucke <hucke@hucke-media.de>
2015-05-20 13:38:24 +0100

    first commit

---
+++ hucke_test/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# This file is part of OpenERP. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
+
+{
+    'name' : 'Test Module Name',
+    'version': '1.0',
+    'author': 'Hucke Media GmbH & Co. KG',
+    'category': 'Custom',
+    'website': 'http://www.hucke-media.com',
+    'summary': 'Odoo Module Summary',
+    'description': '''
+Odoo Module Description
+
+''',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'js': [],
+    'installable': True,
+    'application': False,
+}
---
+++ hucke_tutorial_1/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# This file is part of OpenERP. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
+
+{
+    'name' : 'Hucke Programming Tutorial 1',
+    'version': '1.0',
+    'author': 'Hucke Media GmbH & Co. KG',
+    'category': 'Custom',
+    'website': 'http://www.hucke-media.com',
+    'summary': 'Odoo Module Summary',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'js': [],
+    'installable': True,
+    'application': False,
+}
